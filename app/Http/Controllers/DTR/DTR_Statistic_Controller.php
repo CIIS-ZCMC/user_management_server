c6e7030db0ac3e5d6325417ea72b1a08d3f2ae9d
caimor <zcmc.caimor@gmail.com>
2023-11-10 15:11:31 +0800

    merge dtr to UMIS

---
+++ app/Http/Controllers/DTR/DTR_Statistic_Controller.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\DTR;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DTR_Statistic_Controller extends Controller
+{
+    //
+}
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,59 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::controller(App\Http\Controllers\DTR\DTRcontroller::class)->group(
+    function () {
+        Route::get('/ftchdtrfrmdvc', 'Fetch_DTR_from_Device')->name('fetchdtrfromdevice');
+        Route::get('fetchUserDTR', 'FetchUser_DTR')->name('fetchuserdtr');
+        Route::get('generateDtr', 'Generate_DTR')->name('generateDtr');
+        Route::get('/getHolidays', 'Get_Holidays')->name('getHolidays');
+        Route::get('/setHolidays', 'Set_Holidays')->name('setHolidays');
+        Route::get('/modifyHolidays', 'Modify_Holidays')->name('modifyHolidays');
+        Route::get('/viewdtr', 'ViewDTR')->name('viewdtr');
+        Route::get('/dtrutotreport', 'DTR_UTOT_Report')->name('dtrutotreport');
+        Route::get('/testtest', 'test')->name('testtest');
+    }
+);
+
+
+Route::controller(App\Http\Controllers\DTR\BioController::class)->group(
+    function () {
+
+        Route::get('/newRegistration', 'Register_Bio')->name('newregistration');
+        //  Route::get('/pulldatafromdevice', 'FetchAllData_FromDevice')->name('pullingdata');
+        Route::get('/pushuserdatatodevice', 'Fetch_User_ToDevice')->name('pushinguserdata');
+        Route::get('/setuserasadmin', 'Set_User_SuperAdmin')->name('setuserasadmin');
+        Route::get('/pulluserdatafromdevice', 'Fetch_User_FromDevice')->name('pullinguserdata');
+        Route::get('/fetchallbio', 'Fetch_BIO_To_Device')->name('fetchallbio');
+        Route::get('/deleteall', 'Delete_AllBIO_From_Device')->name('deletealldatafromdevice');
+        Route::get('/delete', 'Delete_SpecificBIO_From_Device')->name('deleteuserdatafromdevice');
+        Route::get('/synctime', 'SyncTime')->name('synctdateandtime');
+        Route::get('/enable/disable', 'Enable_OR_Disable')->name('enabledisable');
+        Route::get('/restart/exit', 'Restart_OR_Shutdown')->name('resshut');
+        Route::get('/setTime', 'settime')->name('setTime');
+    }
+);
+
+
+
+Route::controller(App\Http\Controllers\DTR\BioMSController::class)->group(
+    function () {
+        Route::get('/alldevice', 'index')->name('alldevice');
+        Route::get('/registerDevice', 'add_device')->name('registerDevice');
+        Route::get('/testdevice', 'test_device_connection')->name('testdevice');
+        Route::get('/deleteDevice', 'Delete_device')->name('deleteDevice');
+        Route::get('/updateDevice', 'Update_device')->name('updateDevice');
+    }
+);
+
+Route::controller(App\Http\Controllers\DTR\MailController::class)->group(
+    function () {
+        Route::get('/testemail', 'testemail')->name('testemail');
+    }
+);
+
+
 Route::get('/', function () {
     return view('welcome');
 });
