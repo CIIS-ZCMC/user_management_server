a1b53bf56fa8548331e9f0fd4b7cc6b70931188b
Adrian Agcaoili <adrian.zcmc@gmail.com>
2024-01-24 10:51:19 +0800

    update

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -17,7 +17,8 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 
 
-class Helpers {
+class Helpers
+{
     public static function registerSystemLogs($request, $moduleID, $status, $remarks)
     {
         $ip = $request->ip();
@@ -55,11 +56,11 @@ class Helpers {
 
     public static function getDatesInMonth($year, $month, $value)
     {
-        $start  = new DateTime("{$year}-{$month}-01");
-        $end    = new DateTime("{$year}-{$month}-" . $start->format('t'));
+        $start = new DateTime("{$year}-{$month}-01");
+        $end = new DateTime("{$year}-{$month}-" . $start->format('t'));
 
-        $interval   = new DateInterval('P1D');
-        $period     = new DatePeriod($start, $interval, $end->modify('+1 day'));
+        $interval = new DateInterval('P1D');
+        $period = new DatePeriod($start, $interval, $end->modify('+1 day'));
 
         $dates = [];
 
@@ -124,22 +125,23 @@ class Helpers {
 
             $file->move(public_path($FILE_URL), $fileName);
         }
-        
+
         return $fileName;
     }
 
     public static function infoLog($controller, $module, $message)
     {
-        Log::channel('custom-info')->info($controller.' Controller ['.$module.']: message: '.$message);
-    }
-    
-    public static function errorLog($controller, $module, $errorMessage)
-    {
-        Log::channel('custom-error')->error($controller.' Controller ['.$module.']: message: '.$errorMessage);
+        Log::channel('custom-info')->info($controller . ' Controller [' . $module . ']: message: ' . $message);
     }
 
-    public static function ExchangeDutyApproval($assigned_area, $employee_profile_id) {
-        switch($assigned_area['sector']){
+    public static function errorLog($controller, $module, $errorMessage)
+    {
+        Log::channel('custom-error')->error($controller . ' Controller [' . $module . ']: message: ' . $errorMessage);
+    }
+
+    public static function ExchangeDutyApproval($assigned_area, $employee_profile_id)
+    {
+        switch ($assigned_area['sector']) {
             case 'Division':
                 $division_head = Division::find($assigned_area['details']['id'])->chief_employee_profile_id;
                 return ["approve_by" => $division_head];
@@ -150,7 +152,7 @@ class Helpers {
 
             case 'Section':
                 $section = Section::find($assigned_area['details']['id']);
-                if($section->division !== null){
+                if ($section->division !== null) {
                     return ["approve_by" => $section->supervisor_employee_profile_id];
                 }
 
@@ -159,7 +161,7 @@ class Helpers {
 
             case 'Unit':
                 $section = Unit::find($assigned_area['details']['id'])->section;
-                if($section->department_id !== null){
+                if ($section->department_id !== null) {
                     $department = $section->department;
                     return ["approve_by" => $department->head_employee_profile_id];
                 }
