f73543cdcba70cdb53d1e8d34e2f310c250581c4
Kim Horace A. Dolar <kimhorace@zcmcad.local>
2024-01-26 13:17:26 +0800

    TA resource

--- app/Http/Resources/TimeAdjustmentResource.php
+++ app/Http/Resources/TimeAdjustmentResource.php
@@ -27,15 +27,15 @@ class TimeAdjustmentResource extends JsonResource
         $recommendedBy = $employee;
         $approveBy = $employee;
 
-        $dailyTimeRecord = [
-            'id' => $this->id,
-            'biometric_id' => $this->biometric_id,
-            'dtr_date' => $this->dtr_date,
-        ];
+        // $dailyTimeRecord = [
+        //     'id' => $this->id,
+        //     'biometric_id' => $this->biometric_id,
+        //     'dtr_date' => $this->dtr_date,
+        // ];
 
         return [
             'id'                    => $this->id,
-            'daily_time_record'     => $dailyTimeRecord,
+            'daily_time_record'     => $this->dailyTimeRecord ? new DailyTimeRecordResource($this->dailyTimeRecord) : null,
             'employee_profile'      => $employee,
             'recommended_by'        => $recommendedBy,
             'approve_by'            => $approveBy,
