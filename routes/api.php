f05ab38fda0a9cc7a919dfadfae9620e48993b4e
= <=>
2024-03-19 10:00:15 +0800

    update main add my-areas module

--- routes/api.php
+++ routes/api.php
@@ -809,7 +809,11 @@ Route::middleware('auth.cookie')->group(function () {
         });
 
         Route::middleware(['auth.permission:UMIS-EM view-all'])->group(function () {
-            Route::get('area-employees/{id}', 'EmployeeProfileController@areasEmployees');
+            Route::get('my-areas', 'EmployeeProfileController@myAreas');
+        });
+
+        Route::middleware(['auth.permission:UMIS-EM view-all'])->group(function () {
+            Route::get('area-employees/{id}/sector/{sector}', 'EmployeeProfileController@areasEmployees');
         });
 
         Route::middleware(['auth.permission:UMIS-EM update'])->group(function () {
