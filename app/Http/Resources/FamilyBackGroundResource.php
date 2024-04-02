321e03d21a66190876d890f756b9d818f8d56445
= <=>
2024-04-02 16:09:45 +0800

    handle null on decryptData

--- app/Http/Resources/FamilyBackGroundResource.php
+++ app/Http/Resources/FamilyBackGroundResource.php
@@ -14,8 +14,8 @@ class FamilyBackGroundResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $tin_no = $this->decryptData('tin_no');
-        $rdo_no = $this->decryptData('rdo_no');
+        $tin_no = $this->tin_no !== null? $this->decryptData('tin_no'):null;
+        $rdo_no = $this->rdo_no !== null? $this->decryptData('rdo_no'):null;
 
         return [
             'id' => $this->id,
