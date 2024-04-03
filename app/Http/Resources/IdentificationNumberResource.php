aca2e07d68b233345b5c3540b62c5750908eeecd
= <=>
2024-04-03 14:47:02 +0800

    update

--- app/Http/Resources/IdentificationNumberResource.php
+++ app/Http/Resources/IdentificationNumberResource.php
@@ -14,14 +14,14 @@ class IdentificationNumberResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $gsis = $this->decryptData("gsis_id_no");
-        $pag_ibig = $this->decryptData("pag_ibig_id_no");
-        $philhealth = $this->decryptData("philhealth_id_no");
-        $sss = $this->decryptData("sss_id_no");
-        $prc = $this->decryptData("prc_id_no");
-        $tin = $this->decryptData("tin_id_no");
-        $rdo = $this->decryptData("rdo_no");
-        $bank_no = $this->decryptData("bank_account_no");
+        $gsis = $this->gsis_id_no === null? "NONE":$this->decryptData("gsis_id_no");
+        $pag_ibig = $this->pag_ibig_id_no === null? "NONE":$this->decryptData("pag_ibig_id_no");
+        $philhealth = $this->philhealth_id_no === null? "NONE":$this->decryptData("philhealth_id_no");
+        $sss = $this->sss_id_no === null? "NONE":$this->decryptData("sss_id_no");
+        $prc = $this->prc_id_no === null? "NONE":$this->decryptData("prc_id_no");
+        $tin = $this->tin_id_no === null? "NONE":$this->decryptData("tin_id_no");
+        $rdo = $this->rdo_no === null? "NONE":$this->decryptData("rdo_no");
+        $bank_no = $this->bank_account_no === null? "NONE":$this->decryptData("bank_account_no");
 
         return [
             'id' => $this->id,
