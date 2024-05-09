43cbb43d5b42d7264bd7bbc9dc3a66e2fec4ad59
= <=>
2024-05-09 08:26:55 +0800

    cto validations

--- app/Http/Controllers/LeaveAndOverTime/CtoApplicationController.php
+++ app/Http/Controllers/LeaveAndOverTime/CtoApplicationController.php
@@ -314,15 +314,14 @@ class CtoApplicationController extends Controller
                         ->first();
                 }
 
-              
+
                 if ($employee_credit->earned_credit_by_hour === 0) {
                     return 1;
                     $failed[] = $value;
                     $reason[] = 'No overtime credits available.';
                     continue;
                 }
-
-                // Check if the employee has sufficient earned credits for the application
+                
                 if ($employee_credit->earned_credit_by_hour < $value->applied_credits) {
                     $failed[] = $value;
                     $reason[] = 'Insufficient overtime credits available.';
