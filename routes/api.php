ef7a09a1599695b4b586d102ab7459a5167a8d8d
kimdee <dhoracekay@gmail.com>
2024-05-22 11:29:43 +0800

    Scm

--- routes/api.php
+++ routes/api.php
@@ -2107,7 +2107,7 @@ Route::middleware('auth.cookie')->group(function () {
         });
 
         Route::middleware(['auth.permission:UMIS-ScM view-all'])->group(function () {
-            Route::get('schedules-filter-area', 'ScheduleController@FilterByAreaAndDate');
+            Route::get('schedules-filter', 'ScheduleController@FilterByAreaAndDate');
         });
 
         Route::middleware(['auth.permission:UMIS-ScM view-all'])->group(function () {
