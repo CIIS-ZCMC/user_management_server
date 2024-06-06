0fb170a672b5362b105d816c67ea0cd6c37d0ddc
Alyana Barretto <alyanabarretto.zcmc@gmail.com>
2024-06-06 09:09:28 +0800

    update name leave form

--- resources/views/leave_from/leave_application_form.blade.php
+++ resources/views/leave_from/leave_application_form.blade.php
@@ -452,9 +452,10 @@
             
                                         <div style="margin-top: 20px;">
                                             <span style="font-size: 13px; border-bottom: 1px solid #000; display: inline-block; width: 300px; text-transform:uppercase">
-                                                {{ $data->employeeProfile->personalInformation->first_name }}
-                                                {{ substr($data->employeeProfile->personalInformation->middle_name, 0, 1) }}.
+                                                {{ $data->employeeProfile->personalInformation->employeeName() }}
+                                                {{-- {{ substr($data->employeeProfile->personalInformation->middle_name, 0, 1) }}.
                                                 {{ $data->employeeProfile->personalInformation->last_name }} 
+                                                {{ $data->employeeProfile->personalInformation->last_name }}  --}}
                                             </span>
                                             <br> 
                                             <label style="text-align: center; display: block;">Signature of Applicant</label>
@@ -557,9 +558,9 @@
                                     <div class="text-center" style="padding-top: 20px;">
                                         <span style="font-size: 13px; border-bottom: 1px solid #000; display: inline-block; width: 300px; text-transform:uppercase">
                                             @if ($data->hrmoOfficer)
-                                            {{ $data->hrmoOfficer->personalInformation->first_name }}
-                                            {{ substr($data->hrmoOfficer->personalInformation->middle_name, 0, 1) }}.
-                                            {{ $data->hrmoOfficer->personalInformation->last_name }} 
+                                            {{ $data->hrmoOfficer->personalInformation->employeeName() }}
+                                            {{-- {{ substr($data->hrmoOfficer->personalInformation->middle_name, 0, 1) }}.
+                                            {{ $data->hrmoOfficer->personalInformation->last_name }}  --}}
                                         @endif
                                             {{-- {{ $hrmo_officer->supervisor->personalInformation->first_name }}
                                             {{ substr($hrmo_officer->supervisor->personalInformation->middle_name, 0, 1) }}
@@ -604,9 +605,9 @@
                                     <div style="" class="text-center">
                                         <span style="font-size: 13px; padding-top:40px; border-bottom: 1px solid #000; display: inline-block; width: 300px; text-transform:uppercase">
                                         @if ($data->recommendingOfficer)
-                                            {{ $data->recommendingOfficer->personalInformation->first_name }}
-                                            {{ substr($data->recommendingOfficer->personalInformation->middle_name, 0, 1) }}.
-                                            {{ $data->recommendingOfficer->personalInformation->last_name }} 
+                                            {{ $data->recommendingOfficer->personalInformation->employeeName() }}
+                                            {{-- {{ substr($data->recommendingOfficer->personalInformation->middle_name, 0, 1) }}.
+                                            {{ $data->recommendingOfficer->personalInformation->last_name }}  --}}
                                         @endif
                                       
                                         </span>
@@ -689,9 +690,9 @@
                         <span style="font-size: 13px; border-bottom: 1px solid #000; display: inline-block; width: 250px; padding-top: 30px; text-transform:uppercase">
                             <b>
                                 @if ($data->approvingOfficer)
-                                {{ $data->approvingOfficer->personalInformation->first_name }}
-                                {{ substr($data->approvingOfficer->personalInformation->middle_name, 0, 1) }}.
-                                {{ $data->approvingOfficer->personalInformation->last_name }} 
+                                {{ $data->approvingOfficer->personalInformation->employeeName() }}
+                                {{-- {{ substr($data->approvingOfficer->personalInformation->middle_name, 0, 1) }}.
+                                {{ $data->approvingOfficer->personalInformation->last_name }}  --}}
                                 @else
                                 &nbsp;&nbsp;
                             @endif
