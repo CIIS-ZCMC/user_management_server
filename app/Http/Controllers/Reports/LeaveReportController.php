8e6d935b1943ae796931a319b020a2e69c6c723b
dennisfalcasantosnatividad@gmail.com <dennisfalcasantosnatividad@gmail.com>
2024-06-12 11:39:58 +0800

    new model

---
+++ app/Http/Controllers/Reports/LeaveReportController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Reports;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LeaveReportController extends Controller
+{
+    private $CONTROLLER_NAME = 'Leave Reports';
+}
