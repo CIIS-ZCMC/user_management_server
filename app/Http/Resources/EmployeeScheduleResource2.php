f782da0734872cb6336ccd860dcef773656fcdd0
kimdee <dhoracekay@gmail.com>
2024-07-04 13:13:23 +0800

    new updates E-ScM

--- app/Http/Resources/EmployeeScheduleResource2.php
+++ app/Http/Resources/EmployeeScheduleResource2.php
@@ -14,7 +14,7 @@ class EmployeeScheduleResource2 extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        // Process the schedule data
+        // Process the schedule data 
         $schedule = $this->resource->map(function ($value) {
             return [
                 'id' => $value->id,
@@ -26,13 +26,30 @@ class EmployeeScheduleResource2 extends JsonResource
                     'color' => $value->schedule->timeShift->color,
                 ]
             ];
+        })->toArray();
+
+        // Ensure $schedule is an empty array if there are no schedules
+        $schedule = empty($schedule) ? [] : $schedule;
+
+
+        // Extract schedule dates
+        $scheduleDates = array_map(function ($schedule) {
+            return $schedule['date'];
+        }, $schedule);
+
+        $totalWorkingHours = collect($schedule)->sum(function ($item) {
+            return $item['time_shift']['total_hour'];
         });
+
+        $monthlyWorkingHours = count($scheduleDates) > 0 ? $this->resource->first()->schedule->first()->monthlyWorkingHours($scheduleDates[0]) : 0;
 
         return [
             'employee_id' => $this->resource->isEmpty() ? null : $this->resource->first()->employee_profile_id,
-            'position_type' => $this->resource->first()->employee->findDesignation()->position_type,
-            'shifting' => $this->resource->first()->employee->shifting,
-            'schedule' => $schedule,
+            'position_type' => $this->resource->isEmpty() ? null : optional($this->resource->first()->employee->findDesignation())->position_type,
+            'shifting' => $this->resource->isEmpty() ? null : optional($this->resource->first()->employee)->shifting,
+            'total_working_hours' => $this->resource->isEmpty() ? null : $totalWorkingHours,
+            'monthly_working_hours' => $this->resource->isEmpty() ? null : $monthlyWorkingHours,
+            'schedule' => $this->resource->isEmpty() ? [] : $schedule,
         ];
     }
 }
