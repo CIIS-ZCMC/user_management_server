7b449a7837d9163c2466a3ccbc087a08a786f4dd
TjayAmit <tristanjayamit0813@gmail.com>
2025-02-13 10:04:30 +0800

    added reset password created by DENNIS NATIVIDAD FALCASANTOS

--- app/Http/Controllers/Migration/ResetPasswordWithCsv.php
+++ app/Http/Controllers/Migration/ResetPasswordWithCsv.php
@@ -10,71 +10,77 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
 
 use function Laravel\Prompts\select;
 
 class ResetPasswordWithCsv extends Controller
 {
-    public function resetAndSendNewCredentialToUsers()
+    public function resetAndSendNewCredentialToUsers(Request $request)
     {
-        $emm = null;
         try {
-            // dd(EmployeeProfile::all());
-            $employees = EmployeeProfile::all();
-            // $employees = EmployeeProfile::offset(450)->limit(100)->get();
+            $employeeIds = $request->input('employee_ids'); // Laravel automatically handles JSON
+            $employees = EmployeeProfile::whereIn('id', $employeeIds)->get();
+
             $temp = [];
-            $employeeProfiles = EmployeeProfile::leftJoin('assigned_areas as aa', 'employee_profiles.id', '=', 'aa.employee_profile_id')
-                ->select('employee_profiles.employee_id', 'aa.*')
-                ->where(function ($query) {
-                    $query->where('aa.section_id', 1);
-                })
-                ->get()->pluck('employee_profile_id');
+            $failed_emails = [];
 
 
             foreach ($employees as $employee) {
                 $password = Helpers::generatePassword();
                 $hashPassword = Hash::make($password . config('app.salt_value'));
+                
 
-                // $temp[] = ['id' => $employee->employee_id, 'pass' => $password];
-                if (in_array($employee->id, [2663])) {
+                $employee->authorization_pin = null;
+                $employee->password_encrypted = Crypt::encryptString($hashPassword);
+                $employee->save();
+                // $default_password = Helpers::generatePassword();
+                $data = [
+                    'employeeID' => $employee->employee_id,
+                    'Password' => $password,
+                    "Link" => config('app.client_domain')
+                ];
 
-                    $employee->authorization_pin = null;
-                    $employee->password_encrypted = Crypt::encryptString($hashPassword);
-                    $employee->save();
-                    $employee_profile = EmployeeProfile::find($employee->id);
-                    // $default_password = Helpers::generatePassword();
-                    $data = [
-                        'employeeID' => $employee_profile->employee_id,
-                        'Password' => $password,
-                        "Link" => config('app.client_domain')
-                    ];
-                    if ($employee_profile->personalinformation->contact == null) {
-                        continue;
-                    }
-                    $email = $employee_profile->personalinformation->contact->email_address;
-                    if ($email == 'd@gmail.com') {
-                        continue;
-                    }
-
-                    $name = $employee_profile->personalInformation->name();
-
-
-                    SendEmailJob::dispatch('new_account', $email, $name, $data);
-                    $temp[] = ['sent' => $email];
-                    \Log::info('RESET', [
-                        'employeeID' => $employee->employee_id,
-                        'password' => $password,
-                    ]);
+                if ($employee->personalinformation->contact == null) {
+                    continue;
                 }
+                
+                $email = $employee->personalinformation->contact->email_address;
+
+                if ($email == 'd@gmail.com' || $email === null) {
+                    $failed_emails[] = [
+                        "employee_id" => $employee->employee_id,
+                        "email" => $email
+                    ];
+                    continue;
+                }
+
+                $name = $employee->personalInformation->name();
+
+                SendEmailJob::dispatch('new_account', $email, $name, $data);
+                $temp[] = ['sent' => $email];
+                \Log::info('RESET', [
+                    'employeeID' => $employee->employee_id,
+                    'password' => $password,
+                ]);
             }
 
-            \Log::info('P-Reset&sent Successfully', ["message" => '-------------------------------------------------------------------------------------------']);
-            // dd($temp);
+            if(count($failed_emails) > 0){
+                return response()->json([
+                    'data' => $temp,
+                    "failed_emails" => $failed_emails,
+                    "message" => "Successfully send new account credentials to selected employees. Some email failed to send with reason of email is doesn't exist."
+                ], Response::HTTP_OK);
+            }
+
+            return response()->json([
+                'data' => $temp,
+                "message" => "Successfully send new account credentials to selected employees."
+            ], Response::HTTP_OK);
         } catch (\Throwable $th) {
-            // dd($th);
             return response()->json([
                 'message' => $th->getMessage()
-            ]);
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 }
