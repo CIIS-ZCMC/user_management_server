3f7b1b7a1687ffb1e5549f99fef6193971f808b1
= <=>
2025-08-27 17:00:12 +0800

    fix: result for dailytimerecord handle proper time extraction

--- app/Http/Resources/v2/DailyTimeRecordResource.php
+++ app/Http/Resources/v2/DailyTimeRecordResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources\v2;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Carbon\Carbon;
 
 class DailyTimeRecordResource extends JsonResource
 {
@@ -18,11 +19,16 @@ class DailyTimeRecordResource extends JsonResource
             'id' => $this->id,
             'biometric_id' => $this->biometric_id,
             'dtr_date' => $this->dtr_date,
-            'time_in' => $this->first_in,
-            'break_out' => $this->first_out,
-            'break_in' => $this->second_in,
-            'time_out' => $this->second_out,
+            'time_in' => $this->first_in ? $this->extractTime($this->first_in) : null,
+            'break_out' => $this->first_out ? $this->extractTime($this->first_out) : null,
+            'break_in' => $this->second_in ? $this->extractTime($this->second_in) : null,
+            'time_out' => $this->second_out ? $this->extractTime($this->second_out) : null,
             'overtime' => $this->overtime
         ];
+    }
+
+    protected function extractTime($time)
+    {
+        return Carbon::parse($time)->format('g:i A');
     }
 }
